e3321a86dcdcf7bb02d02e04d9d56dbbabd361ae
Tate <yongzhao.tan@onfinality.io>
2024-09-15 13:25:31 +0000

    yargs hideBin

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 // Copyright 2020-2024 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: GPL-3.0
 
+import { hideBin } from 'yargs/helpers';
 import yargs from 'yargs/yargs';
 import { yargsOptions } from './config/yargs';
 import { startServer } from './server';
 
-yargs(process.argv.slice(2))
+yargs(hideBin(process.argv))
   .command('$0', 'Run an query-subgraph server', yargsOptions, (argv) => {
     startServer(argv);
   })
