314d03322c8ee47636a7e6c12d6fe5a785f39742
Tate <yongzhao.tan@onfinality.io>
2024-09-18 03:45:46 +0000

    Change the test _meta description.

--- test/subgraph.test.ts
+++ test/subgraph.test.ts
@@ -136,7 +136,7 @@ describe("subgraph plugin test", () => {
     await pool.end();
   });
 
-  describe("_mate plugin", () => {
+  describe("_meta plugin", () => {
     it("query _meta", async () => {
       await Promise.all([
         insertMetadata('genesisHash', '"0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3"'),
